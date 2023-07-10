f009f31d6fad2689291067ac5a220b5f07573014
Youmi-Kim <flordelavidaa@naver.com>
2023-07-11 03:16:21 +0900

    initial commit

---
+++ src/Secret.java
@@ -0,0 +1,6 @@
+public class Secret {
+    static final String url = "jdbc:mysql://localhost/coworking?useUnicode=true&"
+            +"useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    static final String user = "root";
+    static final String pw = "0406";
+}
